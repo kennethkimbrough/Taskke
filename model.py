f07c53de99eb6adbd7be0fe7c1c57c7758be1bb6
Kenneth Kimbrough <146112017+kennethkimbrough@users.noreply.github.com>
2024-08-23 18:48:41 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-rom sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, Text, DateTime
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
