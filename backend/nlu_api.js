f0a800f4b7cd99c2970a26587f18b167277a528b
Kenneth Kimbrough <146112017+kennethkimbrough@users.noreply.github.com>
2024-08-24 06:20:37 -0400

    Update nlu_api.js

--- backend/nlu_api.js
+++ backend/nlu_api.js
@@ -15,3 +15,30 @@ app.post('/process_input', (req, res) => {
 app.listen(3001, () => {
   console.log('Rasa NLU API listening on port 3001');
 });
+const express = require('express');
+const app = express();
+const pythonShell = require('python-shell');
+
+app.post('/process_input', (req, res) => {
+  const input = req.body.input;
+  const python = new pythonShell('rasa_nlu.py');
+
+  python.send(input);
+  python.on('message', (response) => {
+    res.json({ response: response });
+  });
+});
+
+app.post('/chat', (req, res) => {
+  const input = req.body.message;
+  const python = new pythonShell('rasa_nlu.py');
+
+  python.send(input);
+  python.on('message', (response) => {
+    res.json({ response: response });
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Rasa NLU API listening on port 3001');
+});
